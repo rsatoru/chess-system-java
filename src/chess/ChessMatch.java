ebcc917311d691a0d4e8d584b9712fbde72fdb69
Rafael Satoru <rsatoru@minsait.com>
2022-02-02 20:30:51 -0300

    Fix:  -  better treatment ro promotion

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,6 +1,5 @@
 package chess;
 
-import java.security.InvalidParameterException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -111,7 +110,7 @@ public class ChessMatch {
 	public ChessPiece replacePromotedPiece(String type) {
 		if(promoted == null) throw new IllegalStateException("There is no piece to be promoted");
 		if(!type.equals("B") && !type.equals("N") && !type.equals("R") && !type.equals("Q")) {
-			throw new InvalidParameterException("Invalid type for promotion");
+			return promoted;
 		}
 		
 		Position pos = promoted.getCheesPosition().toPosition();
